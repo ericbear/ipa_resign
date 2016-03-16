b4197a94b3257b22590d3ec95b596e2e5dec198e
Ericbear <ericbear1130@gmail.com>
2016-03-16 15:55:53 +0800

    ADD: remove Payload and add comments

--- resign.sh
+++ resign.sh
@@ -10,15 +10,24 @@ fi
 pushd $(dirname $IPA)
 
 rm -rf Payload
+
+#extract the IPA
 unzip $IPA
 APP_NAME=$(ls Payload)
 
+#remove useless stuff
 rm -rf "Payload/$APP_NAME/_CodeSignature" "Payload/$APP_NAME/CodeResources"
 
+#replace the mobile provision
 cp $MOBILE_PROVISION "Payload/$APP_NAME/embedded.mobileprovision"
 
+#code signing
 codesign -f -s "$CERT_NAME" "Payload/$APP_NAME"
 
+#pack the IPA
 zip -qr "${IPA}_$(date +%s)" Payload
+
+#remove the Payload
+rm -rf Payload
 
 popd
